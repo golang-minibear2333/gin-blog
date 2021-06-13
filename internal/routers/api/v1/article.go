56eac52db57bdaf9a2de873cf30a459074f80fe6
minibear2333 <pzqu@qq.com>
2021-06-13 21:47:06 +0800

    feat: tcp 响应处理

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -1,6 +1,10 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/golang-minibear2333/gin-blog/pkg/app"
+	"github.com/golang-minibear2333/gin-blog/pkg/errcode"
+)
 
 type Article struct{}
 
@@ -8,8 +12,13 @@ func NewArticle() Article {
 	return Article{}
 }
 
-func (a Article) Get(c *gin.Context)    {}
-func (a Article) List(c *gin.Context)   {}
+func (a Article) Get(c *gin.Context)    {
+}
+func (a Article) List(c *gin.Context)   {
+	// 接口测试 http://localhost:8000/api/v1/article
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+	return
+}
 func (a Article) Create(c *gin.Context) {}
 func (a Article) Update(c *gin.Context) {}
 func (a Article) Delete(c *gin.Context) {}
